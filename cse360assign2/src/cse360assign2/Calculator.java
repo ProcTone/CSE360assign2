0ab0a043679edfdca5a7b9601f1df9734e0e292b
holgu <holgu@matthews-air>
2019-02-23 13:47:39 -0700

    Initial Calculator

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,48 @@
+/* Matthew Holguin
+ * 406
+ * Simulates the operations of a calculator
+ * 
+ */
+package cse360assign2;
+
+//contains the functions for calculator operations
+public class Calculator {
+
+	//keeps track of total value
+	private int total;
+	
+	//calculator constructor that initiates the total value
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	//gets the total value
+	public int getTotal () {
+		return 0;
+	}
+	
+	//adds value to total
+	public void add (int value) {
+		
+	}
+	
+	//subtracts value from total
+	public void subtract (int value) {
+		
+	}
+	
+	//multiplies total by value
+	public void multiply (int value) {
+		
+	}
+	
+	//divides total by value
+	public void divide (int value) {
+		
+	}
+	
+	//gets history of past operations used
+	public String getHistory () {
+		return "";
+	}
+}
