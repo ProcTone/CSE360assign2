73395692d7d2685558e5af273e4baa7de567dc44
holgu <holgu@matthews-air>
2019-02-23 15:15:58 -0700

    Third Version that implements the getHistory method

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -11,9 +11,13 @@ public class Calculator {
 	//keeps track of total value
 	private int total;
 	
+	//history string used to keep track of history operations
+	private String historyString;
+	
 	//calculator constructor that initiates the total value
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
+		historyString = "0";
 	}
 	
 	//gets the total value
@@ -24,30 +28,35 @@ public class Calculator {
 	//adds value to total
 	public void add (int value) {
 		total = total + value;
+		historyString = historyString + " + " + value;
 	}
 	
 	//subtracts value from total
 	public void subtract (int value) {
 		total = total - value;
+		historyString = historyString + " - " + value;
 	}
 	
 	//multiplies total by value
 	public void multiply (int value) {
 		total = total * value;
+		historyString = historyString + " * " + value;
 	}
 	
 	//divides total by value
 	public void divide (int value) {
 		if(value == 0) {
 			total = 0;
+			historyString = historyString + " / 0";
 		}
 		else {
 			total = total / value;
+			historyString = historyString + " / " + value;
 		}
 	}
 	
 	//gets history of past operations used
 	public String getHistory () {
-		return "";
+		return historyString;
 	}
 }
